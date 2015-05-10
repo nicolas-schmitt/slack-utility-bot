976069f84b909334907da68e6470edd45a00bfe3
Hei Lian <heilian@live.se>
2015-05-10 20:46:52 +0000

    add help botling

---
+++ botlings/help.js
@@ -0,0 +1,18 @@
+function HelpManual() {}
+
+HelpManual.prototype.keywords = ["help", "aide"];
+
+HelpManual.prototype.getHelp = function() {
+    return "syntax: help [text]";
+};
+
+HelpManual.prototype.getResponse = function() {
+    return "Available commands : \n" +
+            "htmlencode [text] \n" +
+            "htmldecode [text] \n" +
+            "urlencode [text] \n" +
+            "urldecode [text] \n" +
+            "guid [count] [format] \n";
+};
+
+module.exports = new HelpManual();
